0a528fd7c3305751e707f1175c1f857319f59cf7
ssannejohansson <sannej0hansson@hotmail.com>
2025-10-08 13:21:27 +0200

    Add template structure. Add homepage-template to test functionality

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main import views as index_views 
+
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
