d173b379a485ffb8353c714582806f39119fad8a
ssannejohansson <sannej0hansson@hotmail.com>
2025-10-08 16:07:32 +0200

    Add paths and views to all pages

--- main/views.py
+++ main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def booking(request):
+    return render(request, 'booking.html')
