3358b694194defeea269968f067d3e92aa04fa7c
ssannejohansson <sannej0hansson@hotmail.com>
2025-10-17 16:31:49 +0200

    Bug fix second try

--- my_project/settings.py
+++ my_project/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-z+j2u=b=e=y=xh$)j3-w4vy0zxsz%52!2p84lu#1p+4%9xp2-$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bark-and-brush-dba8d291cd80.herokuapp.com/', '127.0.0.1',]
-CSRF_TRUSTED_ORIGINS = ['https://bark-and-brush-dba8d291cd80.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
+CSRF_TRUSTED_ORIGINS = ['https://bark-and-brush-dba8d291cd80.herokuapp.com']
 
 
 # Application definition
